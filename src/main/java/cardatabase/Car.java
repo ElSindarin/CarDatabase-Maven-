19792bfabb29891a06b697cd7d0608f67163a63c
ElSindarin <53188567+ElSindarin@users.noreply.github.com>
2019-09-26 01:08:33 +0300

    Added some Lombok annotations in Car class.

--- src/main/java/cardatabase/Car.java
+++ src/main/java/cardatabase/Car.java
@@ -1,68 +1,32 @@
 package cardatabase;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
 public class Car {
+    @Getter
     private final String vin;
+    @Getter
+    @Setter
     private String regNumber;
+    @Getter
+    @Setter
     private String brand;
+    @Getter
+    @Setter
     private String model;
+    @Getter
+    @Setter
     private Integer year;
+    @Getter
+    @Setter
     private Integer mileage;
-
-    public Car(String vin, String regNumber, String brand, String model, Integer year, Integer mileage) {
-        this.vin = vin;
-        this.regNumber = regNumber;
-        this.brand = brand;
-        this.model = model;
-        this.year = year;
-        this.mileage = mileage;
-    }
-
-    public String getVin() {
-        return vin;
-    }
-
-    public String getRegNumber() {
-        return regNumber;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public Integer getYear() {
-        return year;
-    }
-
-    public Integer getMileage() {
-        return mileage;
-    }
-
-    public void setRegNumber(String regNumber) {
-        this.regNumber = regNumber;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public void setYear(Integer year) {
-        this.year = year;
-    }
-
-    public void setMileage(Integer mileage) {
-        this.mileage = mileage;
-    }
 
     @Override
     public String toString() {
-        return  "VIN-код: " + vin +
+        return "VIN-код: " + vin +
                 ", Рег. №: " + regNumber +
                 ", Марка: " + brand +
                 ", Модель: " + model +
